368b47ba8730656d14e1e2657d7b6bcb0668a002
Agahan14 <Imamidinov.agahan06@gmail.com>
2024-06-17 16:22:50 +0600

    some changes

--- cms/settings.py
+++ cms/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = "cms.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql_psycopg2",
